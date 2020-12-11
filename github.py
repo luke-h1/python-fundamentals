2a6264809172a3fc5f51c19a448bea78c2a99291
luke-h1 <luke.howsam@yahoo.com>
2020-12-11 12:38:32 +0000

    filter gh results

--- github.py
+++ github.py
@@ -1,9 +1,16 @@
 import pip._vendor.requests as requests
 
+def create_query(languages, min_stars=50000):
+    query = f'stars:>{min_stars} '
+    for language in languages: 
+        query += f'language:{language} '
 
-def repos_with_most_stars():
+    return query
+
+def repos_with_most_stars(languages):
     API_URL = 'https://api.github.com/search/repositories'
-    parameters = {'q': 'stars:>50000'}
+    query = create_query(languages)
+    parameters = {'q': query, "sort": 'stars', 'order': 'desc'}
     response = requests.get(API_URL, params=parameters)
     response_json = response.json()['items']
     return response_json
@@ -11,9 +18,11 @@ def repos_with_most_stars():
 
 if __name__ == '__main__':
     # main method goes here
-    results = repos_with_most_stars()
+    languages = ['python', 'javascript']
+    results = repos_with_most_stars(languages)
     for result in results: 
         language = result['language']
         stars = result['stargazers_count']
         name = result['name']
         print(f'-> {name} is a {language} repo with {stars} stars')
+
